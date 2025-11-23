fb9797e04c67b8bb500bdc85bac237bbd46b836f
Bùi Nguyễn Hoàng Thọ <thohnb@gmail.com>
2025-11-23 23:27:03 +0700

    Fix: 12_V12:Wrong syntax

--- docker/12_V12_drop_status_from_schedules.sql
+++ docker/12_V12_drop_status_from_schedules.sql
@@ -1,11 +1,9 @@
 -- V12: Drop status column from tutor_schedule and student_schedule tables
 -- Lý do: Status column không cần thiết trong schedule tables
-
 -- Drop status column from tutor_schedule
-ALTER TABLE `tutor_schedule`
-DROP COLUMN IF EXISTS `status`;
+ALTER TABLE tutor_schedule
+DROP COLUMN `status`;
 
 -- Drop status column from student_schedule
-ALTER TABLE `student_schedule`
-DROP COLUMN IF EXISTS `status`;
-
+ALTER TABLE student_schedule
+DROP COLUMN `status`;
